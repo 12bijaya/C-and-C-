ee577b53a8b156a02528e6e0a86df057f69ee69f
12bijaya <bijayakumal819@gmail.com>
2025-10-21 14:40:12 +0545

    tkinter: Geometry, maxsize, minsize

---
+++ python/tkinter/01_tinkter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+
+root.geometry("1000x8000")
+root.minsize(1200,2000)
+root.maxsize(1300,4000)
+
+content = Label(text="Hi this is Some one but actually noone")
+content.pack()
+
+root.mainloop()
